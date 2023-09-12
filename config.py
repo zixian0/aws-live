6f9b47a3be050cab3925a4000c320e9eea5d423b
How Zi Xian <141216021+zixian0@users.noreply.github.com>
2023-09-12 21:51:51 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.crdowazmqmbg.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "howzixian-employee"
+customregion = "us-east-1"
+
